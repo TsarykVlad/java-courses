fe2a9a7b820c9a1802a18c4731d1bdd7c26784a7
TsarykVlad <tsarick.vlad@gmail.com>
2017-07-26 22:10:31 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -3,5 +3,11 @@ public class Calculate
 	public static void main(String[] arg)
 	{
 		System.out.println("Calculate...");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double summ = first + second;
+		double dir = first / second;	
+		System.out.println("Sum " + summ);	
+		System.out.println("Dir " + dir);
 	}
 }
